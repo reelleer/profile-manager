93155c242728325a9b9e307019c5f17c92f1ad87
Rolando Espinosa <reel.leer@outlook.com>
2021-09-10 14:39:28 -0600

    Add profile components

--- pm.webapp/src/router/index.js
+++ pm.webapp/src/router/index.js
@@ -16,6 +16,16 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/profile",
+    name: "Perfil",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Profile.vue"),
+  },
+
 ];
 
 const router = createRouter({
