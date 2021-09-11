92854398bfdf9e4a320a3073a8df738197e1d86f
Rolando Espinosa <reel.leer@outlook.com>
2021-09-10 19:27:07 -0600

    Handle input

--- pm.webapp/src/components/PollForm.vue
+++ pm.webapp/src/components/PollForm.vue
@@ -1,29 +1,54 @@
 <template>
   <h2 class="h2 border-bottom border-dark py-2">Club Erasmus</h2>
-  <div class="row row-cols-1 row-cols-md-2">
+  <div @input="save" class="row row-cols-1 row-cols-md-2">
     <div class="col">
-      <textarea class="form-control mb-3" id="opportunities" placeholder="¿Qué oportunidades le ha brindado el programa Erasmus?"></textarea>
+      <textarea v-model="form.opportunities" class="form-control mb-3" id="opportunities" placeholder="¿Qué oportunidades le ha brindado el programa Erasmus?"></textarea>
     </div>
     <div class="col">
-      <textarea class="form-control mb-3" id="floatingInput" placeholder="¿Qué actividades te gustaría que el Club Erasmus promovido por la Unión Europea en Nicaragua impulsara?"></textarea>
+      <textarea v-model="form.activities" class="form-control mb-3" id="floatingInput" placeholder="¿Qué actividades te gustaría que el Club Erasmus promovido por la Unión Europea en Nicaragua impulsara?"></textarea>
     </div>
     <div class="col">
       <div class="mb-3">
         <label for="contacts">¿Tienes contacto con estudiantes becados a través del Programa Erasmus? ¿En caso positivo cuántos conoces?</label>
-        <input type="number" id="contacts" min="0" max="30" class="form-control" placeholder="Contactos">
+        <input v-model="form.contacts" type="number" id="contacts" min="0" max="30" class="form-control" placeholder="Contactos">
       </div>
     </div>
     <div class="col">
       <div class="form-check form-switch mb-3">
-        <input class="form-check-input" type="checkbox" id="fallower">
-        <label for="fallower">¿Sigues las redes sociales de la DUE?</label>
+        <input v-model="form.follower" class="form-check-input" type="checkbox" id="follower">
+        <label for="follower">¿Sigues las redes sociales de la DUE?</label>
       </div>
     </div>
   </div>
 </template>
 <script>
+import debounce from 'lodash/debounce'
+
 export default {
-  name: 'PollForm'
+  name: 'PollForm',
+  props: {
+    opportunities: { type: String, default: '' },
+    activities: { type: String, default: '' },
+    contacts: { type: Number, default: 0 },
+    follower: { type: Boolean, default: true }
+  },
+  data() {
+    return {
+      form: {
+        opportunities: this.opportunities,
+        activities: this.activities,
+        contacts: this.contacts,
+        follower: this.follower
+      }
+    }
+  },
+  methods: {
+    save: debounce(function () {
+        this.$emit("poll:save", { ...this.form })
+      },
+      5000
+    )
+  }
 }
 </script>
 <style></style>
